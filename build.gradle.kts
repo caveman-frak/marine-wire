ccd9d85321b92865b9e4d0e78c5b465c9962b490
Caveman Frak <tim.pickard@bluegecko.co.uk>
2023-03-14 09:17:45 +0000

    some initial classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,6 @@ repositories {
 
 dependencies {
     implementation(project(":shared"))
+    testImplementation(project(":test"))
     implementation("org.locationtech.spatial4j:spatial4j:0.8")
 }
--- src/test/java/uk/co/bluegecko/marine/wire/geo/ContinentTest.java
+++ src/test/java/uk/co/bluegecko/marine/wire/geo/ContinentTest.java
@@ -1,26 +1,27 @@
 package uk.co.bluegecko.marine.wire.geo;
 
-import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.allOf;
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.co.bluegecko.marine.test.jassert.Conditions.extract;
 
 class ContinentTest {
 
-	Continent africa;
+	Continent foo;
 
 	@BeforeEach
 	void setUp() {
-		africa = Continent.builder().code("F").name("Foo").build();
+		foo = Continent.builder().code("F").name("Foo").build();
 	}
 
 	@Test
 	void testBuilder() {
-		assertThat(africa)
+		assertThat(foo)
 				.has(allOf(
-						new Condition<>(c -> c.code().equals("F"), "Code = F"),
-						new Condition<>(c -> c.name().equals("Foo"), "Name = Foo")));
+						extract(Continent::code, "code", String::equals, "equals", "F"),
+						extract(Continent::name, "name", String::equals, "equals", "Foo")));
 	}
+
 }

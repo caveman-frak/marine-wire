5960a94f197fc99a5a958f06fd32cdde8f3e3db5
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-09-29 18:03:06 +0100

    upgrade java, gradle and library versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,4 @@ dependencies {
     implementation(project(":shared"))
     testImplementation(project(":test"))
     implementation("org.locationtech.spatial4j:spatial4j:0.8")
-    implementation("org.springdoc:springdoc-openapi-starter-common:2.5.0")
 }
